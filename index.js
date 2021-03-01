7c05a5d77669703cf3c704b3d3e2b49c12d428e3
Colin Schlecht <schlechtcolin@yahoo.com>
2021-02-28 23:54:00 -0800

    commit tree 17442cae839ceabfce58c3868c66b947b08a8a22

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  }),
+})
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
